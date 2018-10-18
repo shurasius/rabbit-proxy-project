e2ed04addc33336162cad287cf525f0b5c93f2e3
Aliaksandr Karmyzau <aliaksandr.karmyzau@LeverX.local>
2018-10-18 13:09:43 +0300

    Next commit

--- create-or-update-stack.sh
+++ create-or-update-stack.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-usage="Usage: $(basename "$0") us-west-2 RabbitProject --template-body file://rabbitt_project_optimized.yml --parameters ParameterKey=BuildVersion,ParameterValue=rabbit_artifact_1.${BUILD_NUMBER}.tar  ParameterKey=KeyName,ParameterValue=oregon_pair_1 ParameterKey=InstanceType,ParameterValue=t2.micro ParameterKey=SSHLocation,ParameterValue=0.0.0.0/0 ParameterKey=VPC,ParameterValue=vpc-8811e8f0 ParameterKey=Subnets,ParameterValue="subnet-2dd7f377\\,subnet-560a5f2f\\,subnet-f796e1bc" --capabilities CAPABILITY_NAMED_IAM
+usage="Usage: $(basename "$0") region stack-name [aws-cli-opts]
 where:
   region       - the AWS region
   stack-name   - the stack name
